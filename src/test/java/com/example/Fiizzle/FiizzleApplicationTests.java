d215caf37dbc8bad491b490149c20552bb0a0a1b
JIYOUNG PARK <ziy913@naver.com>
2022-12-15 04:59:47 +0900

    first commit

---
+++ src/test/java/com/example/Fiizzle/FiizzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fiizzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiizzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

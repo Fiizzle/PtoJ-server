58f4f61caeb5ec6c95675ef4d35d8e648a604eca
lsh9295 <lsh929500@gmail.com>
2022-12-19 03:56:08 +0900

    conflict error

--- src/main/java/com/example/Fiizzle/config/BaseResponseStatus.java
+++ src/main/java/com/example/Fiizzle/config/BaseResponseStatus.java
@@ -44,7 +44,7 @@ public enum BaseResponseStatus {
      * 4000 : Database, Server 오류
      */
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
-    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다.");
 
 
     // 5000 : 필요시 만들어서 쓰세요
